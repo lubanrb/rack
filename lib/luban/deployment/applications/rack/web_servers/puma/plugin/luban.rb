fe65dec226bdcabdda18f8f787a370208c0a9274
Chi Man Lei <chi.lei@laxino.com>
2016-11-16 10:09:30 +0800

    Updated plugin luban to be backward compatible with Ruby 1.8; Monkey patched on class Puma::Plugin to ensure backward compatibility with Ruby 1.8

--- lib/luban/deployment/applications/rack/web_servers/puma/plugin/luban.rb
+++ lib/luban/deployment/applications/rack/web_servers/puma/plugin/luban.rb
@@ -9,10 +9,27 @@ unless Kernel.method_defined? :define_singleton_method
   end
 end
 
+if RUBY_VERSION =~ /^1\.8/
+  # Monkey patched to ensure backward compatibility with Ruby 1.8
+  class Puma::Plugin
+    # Matches
+    #  "C:/Ruby22/lib/ruby/gems/2.2.0/gems/puma-3.0.1/lib/puma/plugin/tmp_restart.rb:3:in `<top (required)>'"
+    #  AS
+    #  C:/Ruby22/lib/ruby/gems/2.2.0/gems/puma-3.0.1/lib/puma/plugin/tmp_restart.rb
+    CALLER_FILE = /
+      \A       # start of string
+      .+       # file path (one or more characters)
+      (?=      # stop previous match when
+        :\d+     # a colon is followed by one or more digits
+      )
+    /x
+  end
+end
+
 Puma::Plugin.create do
   def start(launcher)
     launcher.define_singleton_method(:phased_restart) do
-      init_options = { config_files: @options.all_of(:config_files) }
+      init_options = { :config_files => @options.all_of(:config_files) }
       @options.instance_variable_set(:@cur, init_options)
       @options.instance_variable_set(:@set, [init_options])
       @config.load
